5cb9aadbea34d699a088119a7f01371e83d986f5
khadarkhaja <qhadar.mohammad@gmail.com>
2018-03-09 18:27:21 +0100

    initial meetingbot 1

---
+++ meetingBot/src/app/header.component.ts
@@ -0,0 +1,55 @@
+import { MenubarModule } from 'primeng/menubar';
+import {Component , OnInit} from "@angular/core"
+import {MenuItem} from 'primeng/api';
+
+
+@Component({
+    // Tag Name for component
+    selector: "app-header",
+    // Out put for the component
+    template: `<div >
+    <p-menubar [model]="menuitems">
+    <div> Welcome {{showUserName()}} </div> 
+     </p-menubar>
+    </div>`
+})
+export class HeaderComponent implements OnInit{
+    menuitems:MenuItem[];
+    hdrUserName:String;
+    adminMenuItems:MenuItem[];
+
+    constructor(){
+        this.hdrUserName = "Guest";
+    }
+
+    showUserName():String{
+        // this.hdrUserName = this.ls.getUserName();
+        return this.hdrUserName;
+    }
+    ngOnInit(){
+        this.menuitems = [
+            {label: 'Home',    routerLink:['/'] , routerLinkActiveOptions:{exact:true}},
+            {label: 'Sign In', routerLink:['/SignIn']},
+            {label: 'Sign Up', routerLink:['/SignUp']},
+            {label: 'Shopping List', routerLink:['/list']}
+
+    ]
+
+//     this.adminMenuItems = [
+//         {label: 'Home',    routerLink:['/'] , routerLinkActiveOptions:{exact:true}},
+//         {label: 'Sign Out', routerLink:['/SignOut']},
+//         {label: 'Shopping List', routerLink:['/list']},
+//         {label: 'Manage products', routerLink:['/manage']}
+
+// ]
+    }
+
+    isUserLoggedIn(){
+        // return !this.ls.getIsLoggedin();
+    }
+
+    isAdminLoggedIn(){
+        // return this.ls.getIsLoggedin();
+    }
+
+}
